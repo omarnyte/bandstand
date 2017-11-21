defef0932ffab3a75b7e02d45687d582c69132f1
Omar <omar.dlsg@gmail.com>
2017-11-20 21:34:00 -0800

    Configures webpack and sets up frontend directory and subdirectories

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,8 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   namespace :api, defaults: {format: :json} do
+    root to: 'static_pages#root' 
+
     resources :bands, only: [:index, :show, :create]
     resource :session, only: [:create, :destroy]
   end
