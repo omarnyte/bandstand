3a2e21bdd728b4b625d1a9382cb40cb87a078af8
Omar <omar.dlsg@gmail.com>
2017-11-22 18:08:18 -0800

    Finishes basic styling for the header

--- frontend/components/navbar/navbar.jsx
+++ frontend/components/navbar/navbar.jsx
@@ -4,20 +4,29 @@ import { Link } from 'react-router-dom';
 // will only be rendered if a user is logged in
 const Navbar = ({ currentBand, logout }) => {
   const display = currentBand ? (
-    <div>
+    <div className='right-nav'>
       <p>Hello, {currentBand.band_name}</p>
       <button onClick={logout}>log out</button>
     </div>
   ) : (
-    <ul className='session-links'>
-      <Link to='/discover' className='session-link'>discover</Link>
-      <Link to='/signup' className='session-link'>sign up</Link>
-      <Link to='/login' className='session-link'>login</Link>
-    </ul>
+    <div className='right-nav'>
+      <input
+        className='temp-search-bar'
+        type='text'
+        placeholder='Search for artist, track, or album'/>
+      <ul className='session-links'>
+        <Link to='/discover' className='session-link'>discover</Link>
+        <Link to='/signup' className='session-link'>sign up</Link>
+        <Link to='/login' className='session-link'>login</Link>
+      </ul>
+    </div>
   );
   return (
     <nav className='navbar'>
-      <h1 className='logo'>bandstand[logo]</h1>
+      <div className='left-nav'>
+        <img className='logo' src="http://www.drunkenwerewolf.com/wp-content/uploads/2017/04/Bandcamp.png"/>
+        <h2 className='slogan'>Discover amazing new music and directly support the artits who make it</h2>
+      </div>
       <div>
         {display}
       </div>
