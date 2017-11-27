86dee9dd8f2697884e460cda034544045b61b3dc
Omar <omar.dlsg@gmail.com>
2017-11-27 13:14:49 -0800

    Creates responsive song list items

--- frontend/components/songs/song_index_item.jsx
+++ frontend/components/songs/song_index_item.jsx
@@ -3,15 +3,26 @@ import React from 'react';
 class SongIndexItem extends React.Component {
   constructor(props) {
     super(props);
+    this.handleClick = this.handleClick.bind(this);
   }
+
+  handleClick(e) {
+    alert(`clicked ${e.target.value}`);
+  }
+
 
   render () {
     const { song } = this.props;
 
     return (
-      <li>
-        {song.title}
-      </li>
+      <div className='song-index-item-component'>
+        <li
+          className='song-index-item'
+          value={song.id}
+          onClick={this.handleClick}>
+          {song.title}
+        </li>
+      </div>
     );
   }
 }
